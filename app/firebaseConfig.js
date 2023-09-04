e4c625d5ab6f7cfabe219752d09525c0f7a0f305
Lewis Benson <tm.lewis.benson@gmail.com>
2023-09-04 14:35:19 -0400

    setup firebase config

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyDYZKujjByiESNTKGOlvewHWjtlsyiLpx0',
   authDomain: 'bus-routes-1c7da.firebaseapp.com',
@@ -13,5 +10,7 @@ const firebaseConfig = {
   appId: '1:829477148266:web:59155ea97847c21dc11c13',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export default app;
